887f016d6a58fd1b03ef9f9e57414f240a28797b
DizygoticCode <rob.noyce@gmail.com>
2025-10-26 15:05:49 +0000

    Make non-preview attachments clickable

--- src/models/message.js
+++ src/models/message.js
@@ -15,6 +15,7 @@ const messageSchema = new mongoose.Schema({
   timestamp: { type: Date, default: Date.now, index: true }, // index for sorting/pagination
   fileUrl: { type: String },
   fileType: { type: String },
+  fileName: { type: String },
   status: { type: String, enum: ["sent", "delivered", "read"], default: "sent" },
   reactions: { type: [reactionSchema], default: [] },   // structured reactions
   pinned: { type: Boolean, default: false },            // pin messages
