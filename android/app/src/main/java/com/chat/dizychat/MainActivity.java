6b927452d8c7321aa33ca203ca1852fe5804b69b
DizygoticCode <rob.noyce@gmail.com>
2026-09-04 13:32:46 +0100

    feat: register Android secure session plugin

--- android/app/src/main/java/com/chat/dizychat/MainActivity.java
+++ android/app/src/main/java/com/chat/dizychat/MainActivity.java
@@ -1,5 +1,13 @@
 package com.chat.dizychat;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(SecureSessionPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
